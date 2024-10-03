993772f4179f13d5b3ffc665e08c14a74316ffc5
maryam rezvani <rezvani.maryam.402@gmail.com>
2024-10-03 12:31:30 +0330

    editing shop modals

--- core/shop/models.py
+++ core/shop/models.py
@@ -11,6 +11,7 @@ class ProductStatusType(models.IntegerChoices):
 class ProductCategoryModel(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(allow_unicode=True,unique=True)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, related_name='category_child')
     
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
@@ -25,7 +26,7 @@ class ProductCategoryModel(models.Model):
 # Create your models here.
 class ProductModel(models.Model):
     user = models.ForeignKey("accounts.User",on_delete=models.PROTECT)
-    category = models.ManyToManyField(ProductCategoryModel)
+    category = models.ForeignKey(ProductCategoryModel, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     slug = models.SlugField(allow_unicode=True,unique=True)
     image = models.ImageField(default="/default/product-image.png",upload_to="product/img/")
