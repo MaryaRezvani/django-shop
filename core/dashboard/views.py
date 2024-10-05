46b546c60deb113bac1e5068e6576f6d27d6e434
Maryam Rezvani <140443757+MaryaRezvani@users.noreply.github.com>
2024-10-05 10:05:31 +0330

    Update views.py
    showing admin dashboard to superusers

--- core/dashboard/views.py
+++ core/dashboard/views.py
@@ -11,8 +11,9 @@ class DashboardHomeView(LoginRequiredMixin,View):
         if request.user.is_authenticated:
             if request.user.type == UserType.customer.value:
                 return redirect(reverse_lazy('dashboard:customer:home'))
-            if request.user.type == UserType.admin.value:
-                return redirect(reverse_lazy('dashboard:admin:home'))
+            if self.request.user.type == UserType.admin.value or \
+               self.request.user.type == UserType.superuser.value:
+                   return redirect(reverse_lazy('dashboard:admin:home'))
         else:
             return redirect(reverse_lazy('accounts:login'))
         return super().dispatch(request, *args, **kwargs)
